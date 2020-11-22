bd95112a5b8edc438da4c0871c1a2c4bb91b5db3
Orange <kavunandrej1998@gmail.com>
2020-11-22 21:06:07 +0300

    add index

--- js/my.js
+++ js/my.js
@@ -2,42 +2,37 @@ function changeStyle (element) {
     element.classList.toggle("edited");
 }
 
-
-
 function addElement () {
     const extraInner = document.createElement('div');
     extraInner.className = "limit__inner";
     extraInner.id = "extInner";
     const mainContainer = document.querySelector('.limit');
     mainContainer.append(extraInner);
+    extraInner.innerHTML = '<span class="close"></span>';
 }
 
  function myFunction(){
-    
-
     const firstButton = document.querySelector('.btn');
     const secondButton = document.querySelector('.scndbtn');
 
     secondButton.addEventListener('click', () => {
-      
-        
         addElement()
-   
-})
-
-
+    })
 
     firstButton.addEventListener('click', () => {
         const squares = document.querySelectorAll('.limit__inner');
 
         squares.forEach(square => {
             changeStyle(square);
-
         });
     })
-    
-}
 
+    const close = document.querySelector('.close')
+
+    close.addEventListener('click', () => {
+        extInner.remove()
+    })
+}
 myFunction()
 
 
